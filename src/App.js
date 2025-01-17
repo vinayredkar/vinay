92fa3519e89556b3dd5092f73d7bea2d2f11f244
vinayredkar <vinayredkar43@gmail.com>
2025-01-18 00:57:49 +0530

    basic layout completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Certificate from './pages/Certificate';
+import Miscellaneous from './pages/Miscellaneous';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/certificate" element={<Certificate />} />
+        <Route path="/miscellaneous" element={<Miscellaneous />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Projects.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Projects = () => (
+  <div>
+    <h1>My Projects</h1>
+    <p>Here are some of the projects I have worked on.</p>
+  </div>
+);
+
+export default Projects;
